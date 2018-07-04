b3f9e2728416831c9bc32494e1d63facd9c1780e
Ruben Quezada <rdquezada@utpl.edu.ec>
2018-07-04 18:06:02 -0500

    asdf

---
+++ 3 Desarrollo/AlgoritmoGenetico/src/algoritmogenetico/gene.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package algoritmogenetico;
+
+/**
+ *
+ * @author william
+ */
+class gene {
+    int a=1;
+int b=1;
+int c=a+b;
+int as;
+}
